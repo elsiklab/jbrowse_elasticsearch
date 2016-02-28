189981791eae1c4e9030b7c23126216cf603758c
Colin <colin.diesh@gmail.com>
2016-02-28 01:44:22 -0600

    Add functional test

--- test/test.js
+++ test/test.js
@@ -1,5 +1,12 @@
 var assert = require('assert');
-var elasticsearch = require('assert');
+var elasticsearch = require('elasticsearch');
+
+var client = new elasticsearch.Client({
+    host: 'localhost:9200'
+});
+
+
+
 describe('Array', function() {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
@@ -8,3 +15,75 @@ describe('Array', function() {
     });
   });
 });
+
+
+describe('ElasticSearch', function() {
+    describe('#search', function() {
+        it('search for basic gene', function(done) {
+            client.search({
+                index: 'gene',
+                type: 'loc',
+                body: {
+                    "query": {
+                        "multi_match" : {
+                            "type": "phrase_prefix",
+                            "query": "Apple2",
+                            "fields": [ "name", "description" ]
+                        }
+                    }
+                }
+            }).then(function (resp) {
+                assert.equal(resp.hits.hits.length, 2);
+                assert.equal(resp.hits.hits[0]._source.description, "mRNA with CDSs but no UTRs");
+                done();
+            }, function(err) {
+                throw err;
+            });
+        });
+        it('search for variant', function(done) {
+            client.search({
+                index: 'gene',
+                type: 'loc',
+                body: {
+                    "query": {
+                        "multi_match" : {
+                            "type": "phrase_prefix",
+                            "query": "rs17882967",
+                            "fields": [ "name", "description" ]
+                        }
+                    }
+                }
+            }).then(function (resp) {
+                assert.equal(resp.hits.hits.length, 1);
+                assert.equal(resp.hits.hits[0]._source.name, "rs17882967");
+                done();
+            }, function(err) {
+                throw err;
+            });
+        });
+        it('search for description', function(done) {
+            client.search({
+                index: 'gene',
+                type: 'loc',
+                body: {
+                    "query": {
+                        "multi_match" : {
+                            "type": "phrase_prefix",
+                            "query": "Ok!",
+                            "fields": [ "name", "description" ]
+                        }
+                    }
+                }
+            }).then(function (resp) {
+                assert.equal(resp.hits.hits.length, 2);
+                assert.equal(resp.hits.hits[0]._source.description, "Ok! Ok! I get the message.");
+                done();
+            }, function(err) {
+                throw err;
+            });
+        });
+    });
+});
+
+
+

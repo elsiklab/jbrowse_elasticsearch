856d050a25741ccb19c94dc54b469ebd70fa807f
Colin <colin.diesh@gmail.com>
2016-02-28 22:25:14 -0600

    Use static test data

--- bin/test.sh
+++ bin/test.sh
@@ -4,9 +4,9 @@
 curl -XDELETE 'http://localhost:9200/_all?pretty=true'
 
 #load data
-extlib/bin/flatfile-to-json.pl --gff test/volvox.gff3 --trackLabel volvox --trackType CanvasFeatures --nameAttributes name,id,description,note --out test/volvox
-extlib/bin/flatfile-to-json.pl --gff test/volvox.gff3 --type mRNA --trackLabel volvox_transcript --trackType CanvasFeatures --nameAttributes name,id,description,note --out test/volvox
-extlib/bin/prepare-refseqs.pl --fasta test/volvox.fa --out test/volvox
+flatfile-to-json.pl --gff test/volvox.gff3 --trackLabel volvox --trackType CanvasFeatures --nameAttributes name,id,description,note --out test/volvox
+flatfile-to-json.pl --gff test/volvox.gff3 --type mRNA --trackLabel volvox_transcript --trackType CanvasFeatures --nameAttributes name,id,description,note --out test/volvox
+prepare-refseqs.pl --fasta test/volvox.fa --out test/volvox
 
 #generate elastic index
-bin/generate-elastic-search.pl --url http://localhost:4730 --out test/volvox
+bin/generate-elastic-search.pl --out test/volvox

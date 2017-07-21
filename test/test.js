7b0d62c1f9d9d3c0f4a2ea31d59bcdc0ce38547d
Colin <colin.diesh@gmail.com>
2017-07-21 17:31:51 -0400

    return promises

--- test/test.js
+++ test/test.js
@@ -9,37 +9,35 @@ var client = new elasticsearch.Client({
 describe('ElasticSearch', function() {
     describe('#search', function() {
         it('search for basic gene', function(done) {
-            client.search({
+            return client.search({
                 index: 'gene',
                 type: 'loc',
                 body: {
-                    'sort': ['description'],
-                    'query': {
-                        'multi_match': {
-                            'type': 'phrase_prefix',
-                            'query': 'Apple2',
-                            'fields': [ 'name', 'description' ]
+                    sort: ['description'],
+                    query: {
+                        multi_match: {
+                            type: 'phrase_prefix',
+                            query: 'Apple2',
+                            fields: [ 'name', 'description' ]
                         }
                     }
                 }
             }).then(function(resp) {
+                console.error(resp);
                 assert.equal(resp.hits.total, 6);
                 assert.equal(resp.hits.hits[4]._source.description, 'mRNA with CDSs but no UTRs');
-                done();
-            }, function(err) {
-                throw err;
             });
         });
         it('search for variant', function(done) {
-            client.search({
+            return client.search({
                 index: 'gene',
                 type: 'loc',
                 body: {
-                    'query': {
-                        'multi_match': {
+                    query: {
+                        multi_match: {
                             'type': 'phrase_prefix',
-                            'query': 'rs17882967',
-                            'fields': [ 'name', 'description' ]
+                            query: 'rs17882967',
+                            fields: [ 'name', 'description' ]
                         }
                     }
                 }
@@ -47,30 +45,24 @@ describe('ElasticSearch', function() {
                 assert.equal(resp.hits.total, 1);
                 assert.equal(resp.hits.hits[0]._source.name, 'rs17882967');
                 assert.equal(resp.hits.hits[0]._source.track_index, 'volvox_vcf_test');
-                done();
-            }, function(err) {
-                throw err;
             });
         });
         it('search for description', function(done) {
-            client.search({
+            return client.search({
                 index: 'gene',
                 type: 'loc',
                 body: {
-                    'query': {
-                        'multi_match': {
-                            'type': 'phrase_prefix',
-                            'query': 'Ok!',
-                            'fields': [ 'name', 'description' ]
+                    query: {
+                        multi_match: {
+                            type: 'phrase_prefix',
+                            query: 'Ok!',
+                            fields: [ 'name', 'description' ]
                         }
                     }
                 }
             }).then(function(resp) {
                 assert.equal(resp.hits.total, 1);
                 assert.equal(resp.hits.hits[0]._source.description, 'Ok! Ok! I get the message.');
-                done();
-            }, function(err) {
-                throw err;
             });
         });
     });
